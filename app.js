ee811b1613b14f51bd00d465f1eca065970803f7
Justin W Soto <soto@pdx.edu>
2021-07-01 16:13:50 -0700

    added JS

--- app.js
+++ app.js
@@ -1,8 +1,47 @@
-// import functions and grab DOM elements
+
+import { didUserWin } from './utils.js';
+
+
+const playButton = document.querySelector('#play');
+const winsSpan = document.querySelector('#total-wins');
+const lossesSpan = document.querySelector('#total-losses');
 
 // initialize state
 
+let wins = 0;
+let losses = 0;
+
 // set event listeners 
-  // get user input
+
+playButton.addEventListener('click', ()=>{
+    const selected = document.querySelector('input[type=radio]:checked');
+    const userThrow = selected.value;
+
+    let computerThrow = 'rock';
+    let randomNum = Math.floor(Math.random() * 3);
+    if (randomNum === 0) {
+        computerThrow = 'rock';
+    }
+    if (randomNum === 1) {
+        computerThrow = 'paper';
+    }
+    if (randomNum === 2) {
+        computerThrow = 'scissors';
+    }
+    console.log(computerThrow);
+    // check who won
+    const isWinner = didUserWin(userThrow, computerThrow);
+
+    if (isWinner){
+        wins++;
+    } else {
+        losses++;
+    }
+    winsSpan.textContent = wins;
+    lossesSpan.textContent = losses;
+});
+ // get user input
+
+
   // use user input to update state 
   // update DOM to reflect the new state
--- utils.js
+++ utils.js
@@ -1,5 +1,33 @@
-export function didUserWin(userChoice, compChoice) {
-    if (userChoice === compChoice) {
-        return 'draw';
+// userThrow
+// rock, paper, scissors
+
+// computerThrow
+// rock, paper, scissors
+
+// if the userThrow is the same as the computerThrow
+//   return 'draw'
+// if the user throws rock
+//    if computerThrow is scissors return 'win'
+// if the user throws paper
+//    if computerThrow is rock return 'win'
+// if the user throw scissors
+//    if computerThrow is paper return 'win'
+// return 'lose'
+
+export function didUserWin(userThrow, computerThrow) {
+    if (userThrow === computerThrow) {
+        'draw';
+    } if (userThrow === 'rock' && computerThrow === 'scissors') {
+        return 'You Win!';
+    } if (userThrow === 'rock' && computerThrow === 'paper') {
+        return 'You Lose!';
+    } if (userThrow === 'scissors' && computerThrow === 'paper') {
+        return 'You Win!';
+    } if (userThrow === 'scissors' && computerThrow === 'rock') {
+        return 'You Lose!';
+    } if (userThrow === 'paper' && computerThrow === 'scissors') {
+        return 'You Lose!';
+    } if (userThrow === 'paper' && computerThrow === 'rock') {
+        return 'You Win!';
     }
 }
