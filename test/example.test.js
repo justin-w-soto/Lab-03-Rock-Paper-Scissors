6dfc43c098cf350528f3ed6821d90adbf4ed22c2
Justin W Soto <soto@pdx.edu>
2021-07-01 14:02:59 -0700

    initial html, css are setup

--- test/example.test.js
+++ test/example.test.js
@@ -1,12 +1,16 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
-
+import { didUserWin } from "../utils.js";
 const test = QUnit.test;
 
 test('time to test a function', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    const win = didUserWin('rock', 'scissors');
+    const win2 = didUserWin('scissors', 'paper');
+    const win3 = didUserWin('paper', 'rock');
+
+    const loss = didUserWin(); 
     
     //Act 
     // Call the function you're testing and set the result to a const
@@ -14,5 +18,6 @@ test('time to test a function', (expect) => {
 
     //Expect
     // Make assertions about what is expected versus the actual result
-    expect.equal(actual, expected);
+    expect.equal(win, true);
+    expect.equal(loss, false);
 });
---
+++ utils.js
@@ -0,0 +1,5 @@
+export function didUserWin(userChoice, compChoice) {
+    if (userChoice === compChoice) {
+        return 'draw';
+    }
+}
